04f8c50b443b30387d8ff731c0e25a90fafb928c
danielggonzales <39431466-danielggonzales@users.noreply.replit.com>
2025-05-28 22:50:53 +0000

    Restored to 'bde72a5bcb1993b55cea99116829e17decd6784b'
    Replit-Restored-To: bde72a5bcb1993b55cea99116829e17decd6784b

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import PersistentPieBot from "@/components/PersistentPieBot";
 import UserDashboard from "@/pages/UserDashboard";
 import Companies from "@/pages/Companies";
 import CompanyPositions from "@/pages/CompanyPositions";
@@ -40,7 +39,6 @@ function App() {
       <TooltipProvider>
         <Toaster />
         <Router />
-        <PersistentPieBot />
       </TooltipProvider>
     </QueryClientProvider>
   );
